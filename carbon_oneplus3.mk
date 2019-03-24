5a5285f6b59cd384b8983232540b14a45634fe20
Filippo <zzkw35@gmail.com>
2019-03-24 18:11:52 +0100

    oneplus3: Carbon-ify

--- carbon_oneplus3.mk
+++ carbon_oneplus3.mk
@@ -20,13 +20,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit Carbon GSM telephony parts.
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2018-11-01
 
-PRODUCT_NAME := lineage_oneplus3
+PRODUCT_NAME := carbon_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
